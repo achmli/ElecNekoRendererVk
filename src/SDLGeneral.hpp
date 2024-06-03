94aaa0222f6cbfbb6ea987a4e2a7fcad8b0b858c
achmli <503292386@qq.com>
2024-06-03 21:43:31 +0800

    add third party libs

--- src/SDLGeneral.hpp
+++ src/SDLGeneral.hpp
@@ -3,12 +3,13 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_vulkan.h>
 
+#include "DeviceContext.hpp"
 #include <memory>
 #include <cstdio>
 
 struct LoopData
 {
 	SDL_Window* mWindow = nullptr;
-	
+	std::unique_ptr<DeviceContext> device;
 };
 
